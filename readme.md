87078788540d220045b63c3a16c4bad10e318c3a
chiwoo <qkrcldn12@gmail.com>
2020-07-29 15:52:07 +0900

    update

---
+++ readme.md
@@ -0,0 +1,42 @@
+## Azure Storage Account의 API를 사용한 Storage Operation
+
+### 컨텐츠
+현재는 주로 NoSQL 형식의 Storage Table을 위주로 작성되었습니다.
+
+### Table 사용 방법
+
+#### 1. Storage Account 인증
+Storage Account는 이름에 Account가 들어간 리소스 답게 Azure AD인증이 아닌 Storage Account이름, Shared Key 를 사용하여 인증 헤더를 만듭니다.
+따라서 인증 헤더를 암호화할 Storage Account의 Shared Key정보를 입력해야 합니다.
+
+```powershell
+## 모듈 불러오기
+Import-Module .\StorageOperation.psm1 -Force
+Import-Module .\lib\SetAzAD.psm1 -Force
+
+
+## 전역 변수
+$clientCredType = 'client_credentials'
+$clientResource = 'https://management.azure.com'
+
+
+## 환경 설정 json 파일 불러오기
+$config = Get-Content -Path .\config.json -Raw | ConvertFrom-Json
+
+
+## Azure AD access_token 발급받기
+$AzAdInfo = SetAzAD -tenant $config.AzureAD.tenant -subscription $config.AzureAD.subscription
+$AzAdInfo.setToken($clientCredType, $config.AzureAD.clientId, $config.AzureAD.clientSecret, $clientResource)
+
+
+## access_token을 사용하여 Storage의 Shared Key를 조회하여 인증
+$StorageTable = StorageTableFromAD -azAdInfo $azAdInfo `
+                                   -storageAccoutResourceGroup $config.Storage.StorageAccountResourceGroup `
+                                   -storageAccoutName $config.Storage.StorageAccountName
+
+## Storage의 이름과 Shared Key를 직접 사용하여 인증
+$StorageTableByKey = StorageTable -storageAccoutName $config.Storage.StorageAccountName `
+                                  -key $config.Storage.StorageAccountSharedKey
+```
+
+#### 테이블 생성
---
+++ test.ps1
@@ -0,0 +1,43 @@
+Import-Module .\StorageOperation.psm1 -Force
+Import-Module .\lib\SetAzAD.psm1 -Force
+
+$config = Get-Content -Path .\config.json -Raw | ConvertFrom-Json
+$clientCredType = 'client_credentials'
+$clientResource = 'https://management.azure.com'
+
+$AzAdInfo = SetAzAD -tenant $config.AzureAD.tenant -subscription $config.AzureAD.subscription
+$AzAdInfo.setToken($clientCredType, $config.AzureAD.clientId, $config.AzureAD.clientSecret, $clientResource)
+
+## Get Storage Shared Key with AD access_token
+$StorageTable = StorageTableFromAD -azAdInfo $azAdInfo `
+                                   -storageAccoutResourceGroup $config.Storage.StorageAccountResourceGroup `
+                                   -storageAccoutName $config.Storage.StorageAccountName
+
+## Get Storage
+$StorageTableByKey = StorageTable -storageAccoutName $config.Storage.StorageAccountName `
+                                  -key $config.Storage.StorageAccountSharedKey
+
+
+## Storage Operation
+$createStorage = $StorageTable.createTable('Sample')
+$createStorage2 = $StorageTableByKey.createTable('Sample2')
+
+$data = '{  
+    "Address":"Mountain View",
+    "Age":23,
+    "AmountDue":200.23,
+    "CustomerCode@odata.type":"Edm.Guid",
+    "CustomerCode":"c9da6455-213d-42c9-9a79-3e9149a57833",
+    "CustomerSince@odata.type":"Edm.DateTime",
+    "CustomerSince":"2008-07-10T00:00:00",
+    "IsActive":true,
+    "NumberOfOrders@odata.type":"Edm.Int64",
+    "NumberOfOrders":"255",
+    "PartitionKey":"mypartitionkey",
+    "RowKey":"myrowkey"
+}'
+$insertData = $StorageTable.insertData('Sample', $data)
+$insertData2 = $StorageTableByKey.insertData('Sample2', $data)
+
+$query = "RowKey eq 'myrowkey' and NumberOfOrders eq 255"
+$StorageTable.selectByQuery('Sample', $query)
